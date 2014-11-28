1556ef2990826524a78794b01771ff96fe8b93fb
Job Vranish <job.vranish@gmail.com>
2014-11-28 07:30:36 -0500

    almost dome

--- shape_loader.rb
+++ shape_loader.rb
@@ -5,6 +5,10 @@ require_relative 'frame_time'
 
 include Gosu
 
+def manhattan_distance(a, b)
+  (a.x - b.x).abs + (a.y - b.y).abs
+end
+
 LEFT  = Vector2d.new(-1.0,  0.0)
 RIGHT = Vector2d.new( 1.0,  0.0)
 UP    = Vector2d.new( 0.0, -1.0)
@@ -29,52 +33,122 @@ class Shape < Struct.new(:pos,
                          :type)
 end
 
+class Truck < Struct.new(:pos,
+                         :vel,
+                         :shapes_needed,
+                         :shapes_loaded)
+  def step(dt)
+    self.pos += (vel * dt)
+  end
+  def draw(world)
+
+  end
+end
+
 class Loader < Struct.new(:pos,
                           :vel,
                           :bucket_offset,
                           :desired_bucket_offset,
                           :shape)
+  attr_accessor :shape
   def step(dt)
     self.pos += (vel * dt)
-    offset_error = desired_bucket_offset- bucket_offset
-    self.bucket_offset += sign(offset_error) * 0.1
+    offset_error = desired_bucket_offset - bucket_offset
+    if offset_error != ZERO_VECTOR
+      self.bucket_offset += offset_error.normalize * 0.1
+    end
   end
 end
 
+SHAPES = [:triangle, :circle, :square, :diamond]
+
+SHAPES_NEEDED = {
+  triangle: :triangle_needed,
+  circle: :circle_needed,
+  square: :square_needed,
+  diamond: :diamond_needed,
+}
+
+
+
 class World < Struct.new(:loader,
+                         :truck,
                          :shapes,
                          :bounds)
   def initialize(bounds)
+
+    positions = []
+    [1, 3, 5, 7, 9, 11, 13].each do |x| 
+      [1, 3, 5, 7, 9, 11].each do |y| 
+        positions << Vector2d.new(x.to_f, y.to_f)
+      end
+    end
+    positions.shuffle!
+
+    
+    
+
     shapes = (0..3).flat_map do |i|
-      [Shape.new(Vector2d.new(1.0 + i * 2, 1.0), :triangle),
-       Shape.new(Vector2d.new(1.0 + i * 2, 3.0), :circle),
-       Shape.new(Vector2d.new(1.0 + i * 2, 5.0), :square),
-       Shape.new(Vector2d.new(1.0 + i * 2, 7.0), :diamond)]
+      SHAPES.map do |s|
+        Shape.new(positions.shift, s)
+      end
     end.to_a
+
+    shapes_needed = [
+      SHAPES_NEEDED[SHAPES.sample],
+      SHAPES_NEEDED[SHAPES.sample],
+      SHAPES_NEEDED[SHAPES.sample],
+      SHAPES_NEEDED[SHAPES.sample]
+    ]
     # shapes = circles + [ Shape.new(Vector2d.new(1.0, 3.0), :circle), 
     #            Shape.new(Vector2d.new(3.0, 1.0), :circle), 
     #            Shape.new(Vector2d.new(3.0, 3.0), :circle), ]
     super(Loader.new(Vector2d.new(10.0, 10.0),
                      ZERO_VECTOR,
-                     0,
+                     ZERO_VECTOR,
+                     ZERO_VECTOR,
                      nil),
+          Truck.new(Vector2d.new(6.0, 13.0),
+                    ZERO_VECTOR,
+                    shapes_needed,
+                    []),
           shapes,
           bounds)
   end
 
   def try_to_pickup_shape
     if loader.shape.nil?
-      shapes.detect
+      # shapes.detect
+      false
     end
   end
 
   def update(vel, desired_bucket_offset)
     loader.vel = vel
     loader.desired_bucket_offset = desired_bucket_offset
+    shapes.each_with_index do |shape, i|
+
+      if loader.shape.nil? && manhattan_distance(shape.pos + RIGHT, loader.pos) <= 1.0 &&
+          truck.shapes_needed.first == SHAPES_NEEDED[shape.type]
+        loader.shape = shape.type
+        shapes.delete_at(i)
+        break
+      end
+    end
+    if !loader.shape.nil? && manhattan_distance(truck.pos + RIGHT + (truck.shapes_needed.length * LEFT), loader.pos) <= 1.0
+      truck.shapes_loaded << loader.shape
+      loader.shape = nil
+      truck.shapes_needed.shift
+
+      if truck.shapes_needed.empty?
+        truck.vel = Vector2d.new(-1.0, 0.0)
+      end
+    end
   end
 
   def step(dt)
     loader.step(dt)
+    truck.step(dt)
   end
 end
 
@@ -83,8 +157,6 @@ DIRECTIONS = { Gosu::KbLeft  => LEFT,
                Gosu::KbRight => RIGHT,
                Gosu::KbUp    => UP,
                Gosu::KbDown  => DOWN }
-
-
 
 
 class Game < Gosu::Window
@@ -96,15 +168,26 @@ class Game < Gosu::Window
     end
 
     self.caption = "Shape Loader!" 
-    @images = { evoker: Image.new(self, "./gfx/evoker.base.172.png", false),
-                 floor: Image.new(self, "./gfx/flagstone.base.111.png", false),
-                 circle: Image.new(self, "./gfx/circle.png", false),
-                 square: Image.new(self, "./gfx/square.png", false),
-                 triangle: Image.new(self, "./gfx/triangle.png", false),
-                 diamond: Image.new(self, "./gfx/diamond.png", false), }
+    @images = { loader: Image.new(self, "./gfx/loader.png", false),
+                loader_arm: Image.new(self, "./gfx/loader_arm.png", false),
+                loader_scoop: Image.new(self, "./gfx/loader_scoop.png", false),
+                floor: Image.new(self, "./gfx/floor.png", false),
+                circle: Image.new(self, "./gfx/circle.png", false),
+                square: Image.new(self, "./gfx/square.png", false),
+                triangle: Image.new(self, "./gfx/triangle.png", false),
+                diamond: Image.new(self, "./gfx/diamond.png", false),
+                circle_needed: Image.new(self, "./gfx/circle_needed.png", false),
+                square_needed: Image.new(self, "./gfx/square_needed.png", false),
+                triangle_needed: Image.new(self, "./gfx/triangle_needed.png", false),
+                diamond_needed: Image.new(self, "./gfx/diamond_needed.png", false),
+                truck: Image.new(self, "./gfx/truck.png", false), }
     @our_id = nil
+    restart
+  end
+
+  def restart
     @world = World.new(Bounds.new(15, 15))
-    @desired_bucket_offset = 0.0
+    @desired_bucket_offset = Vector2d.new(0.0, 0.0)
 
     @last_player_vel = ZERO_VECTOR
   end
@@ -116,7 +199,7 @@ class Game < Gosu::Window
   def update
     commanded_directions = DIRECTIONS.select{ |key, value| self.button_down? key }.values
     player_vel = commanded_directions.inject(ZERO_VECTOR) { |sum, x| sum + x }
-    @world.update(player_vel * 3.0, @desired_bucket_offset)
+    @world.update(player_vel * 8.0, @desired_bucket_offset)
     @world.step(@frame_time.dt)
 
     # todo ignore sending events with the same velocity
@@ -125,12 +208,20 @@ class Game < Gosu::Window
     #   @connection.send_event(Marshal.dump(player_vel * 5.0))
     # end
     # @last_player_vel = player_vel
+    if @world.truck.pos.x < -2.0
+      restart
+    end
   end
 
   def draw_bucket(loader)
-    bucket_offset = Vector2d.new(-1.0, -0.2 + loader.bucket_offset)
-    pos = bucket_offset + loader.pos
-    images[:circle].draw(pos.x * 32, pos.y * 32, 1)
+    if not loader.shape.nil?
+      bucket_offset = Vector2d.new(-0.32, 0.32) + loader.bucket_offset
+      pos = bucket_offset + loader.pos
+      images[loader.shape].draw(pos.x * 32, pos.y * 32, 1)
+      pos = loader.bucket_offset + loader.pos
+      images[:loader_arm].draw(pos.x * 32, pos.y * 32, 3)
+      images[:loader_scoop].draw(pos.x * 32, pos.y * 32, 3)
+    end
   end
 
   def draw_shape(shape)
@@ -140,14 +231,20 @@ class Game < Gosu::Window
 
   
   def draw
-    @world.bounds.width.times do |x|
-      @world.bounds.height.times do |y|
-        images[:floor].draw(x * 32, y * 32, 0)
-      end
-    end
+    images[:floor].draw(0.0, 0.0, 0)
 
     pos = @world.loader.pos
-    images[:evoker].draw(pos.x * 32, pos.y * 32, 1)
+    images[:loader].draw(pos.x * 32, pos.y * 32, 3)
+    
+
+    pos = @world.truck.pos
+    tpos = pos + (LEFT * 4)
+    images[:truck].draw(tpos.x * 32, tpos.y * 32, 2)
+    truck_shapes = @world.truck.shapes_loaded + @world.truck.shapes_needed
+    truck_shapes.each_with_index do |shape_needed, i|
+      spos = pos + (LEFT * (3-i))
+      images[shape_needed].draw(spos.x * 32, spos.y * 32, 1)
+    end
 
     @world.shapes.each do |shape|
       draw_shape(shape)
@@ -158,7 +255,7 @@ class Game < Gosu::Window
 
     
   #   @world.players.values.each do |p|
-  #     images[:evoker].draw((p.pos.x * 32).round, (p.pos.y * 32).round, 1)
+  #     images[:loader].draw((p.pos.x * 32).round, (p.pos.y * 32).round, 1)
   #   end
   end
 
@@ -168,11 +265,11 @@ class Game < Gosu::Window
 
   def button_down(id)
     if id == Gosu::KbSpace then
-      # if @desired_bucket_offset != -1.0
-      #   @desired_bucket_offset = -1.0
-      # else
-      #   @desired_bucket_offset = 0.0
-      # end
+      if @desired_bucket_offset != Vector2d.new(-0.3, -0.3)
+        @desired_bucket_offset = Vector2d.new(-0.3, -0.3)
+      else
+        @desired_bucket_offset = Vector2d.new(0.0, 0.0)
+      end
 
 
       
